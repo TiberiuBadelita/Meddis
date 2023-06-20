0f8f3667837df6225ecc4081e639dcf8083ded44
TiberiuBadelita <badelita.tibi@yahoo.com>
2023-06-20 17:13:08 +0300

    Changes at data import

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import '../styles/Styles.css';
 import '../styles/HomePage.css';
 import WithAuth from "./WithAuth";
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 import jwt from 'jwt-decode';
 import axios from 'axios';
-
+import Papa from "papaparse";
 
 function HomePage() {
     const location = window.location.pathname;
@@ -19,6 +19,8 @@ function HomePage() {
     const getDeadlineService = "https://localhost:7053/api/allocations/get-deadline";
     const setDeadlineService = "https://localhost:7053/api/allocations/set-deadline/";
     const nav = useNavigate();
+    const [refreshKey, setRefreshKey] = useState(0);
+    const [searchValue, setSearchValue] = useState("");
     const [accounts, setAccounts] = useState([]);
     const [deadline, setDeadline] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -46,7 +48,7 @@ function HomePage() {
 
         fetchAccounts();
         fetchDeadline();
-    }, []);
+    }, [refreshKey]);
 
 
     const handleDelete = async (id) => {
@@ -103,6 +105,160 @@ function HomePage() {
         }
     };
 
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    const searchedAccounts = searchValue === ""
+    ? accounts
+    : accounts.filter(
+        (account) =>
+           (account.firstName != null ? account.firstName.toLowerCase().includes(searchValue.toLowerCase()):false) ||
+            account.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+            account.role.toLowerCase().includes(searchValue.toLowerCase())
+      );
+
+    const fileInputRef = useRef(null);
+
+    const handleFileSelect = (event) => {
+        event.preventDefault();
+        const file = event.target.files[0];
+        if (file && file.type === 'text/csv') {
+          console.log(file);
+        } else {
+          alert('Invalid file type. Please select a CSV file.');
+        }
+
+        Papa.parse(file, {
+            header: true,
+            skipEmptyLines: true,
+            complete: async function(results) {
+                const data = results.data;
+                var ids = [];
+
+                for (let i = 0; i < data.length; i++) {
+                    if(data[i].Role === "Doctor") {
+
+                        var firstName = data[i].FirstName;
+                        var lastName = data[i].LastName;
+                        var role = data[i].Role;
+                        var email = data[i].Email;
+                        var password = "Password123!";
+                        var doctor = {
+                            firstName,
+                            lastName,
+                            role,
+                            email,
+                            password
+                        }
+
+                        await axios.post('https://localhost:7053/api/users/register', doctor)
+                            .then((response) => {
+                                ids[i] = response.data.id;
+                                accounts.push(response.data);
+                            });
+
+                    }
+                    else if(data[i].Role === "Hospital") {
+                         firstName = "";
+                         lastName = data[i].LastName;
+                         role = data[i].Role;
+                         email = data[i].Email;
+                         password = "Password123!";
+                         var hospital = {
+                            firstName,
+                            lastName,
+                            role,
+                            email,
+                            password
+                        }
+
+                        await axios.post('https://localhost:7053/api/users/register', hospital)
+                            .then((response) => {
+                                ids[i] = response.data.id;
+                                accounts.push(response.data);
+                            });
+
+           
+                    }
+                }
+
+                setRefreshKey((oldKey) => oldKey + 1);
+
+                for (let i = 0; i < data.length; i++) {
+                    if(data[i].Role === "Doctor") {
+                        var id = ids[i];
+
+                        if(!data[i].hasOwnProperty("__parsed_extra")) {
+                            accounts.forEach((account) => {
+                                if(account.lastName === data[i].Preferences || account.firstName + " " + account.lastName === data[i].Preferences || data.find((account) => account.lastName === data[i].Preferences) ){
+                                    data[i].Preferences = account.id;
+                                }
+                            });
+
+                            for(let k = 0; k < data.length; k++) {
+                                if(data[k].lastName === data[i].Preferences || data[k].firstName + " " + data[k].lastName === data[i].Preferences) {
+                                    data[i].Preferences = ids[k];
+                                }
+                            }
+                            
+                            var preferences = data[i].ExamGrade + "|" + data[i].Preferences + "|" + data[i].Specialization;
+                        }
+                        else {
+                            accounts.forEach((account) => {
+                                if(account.lastName === data[i].Preferences || account.firstName + " " + account.lastName === data[i].Preferences) {
+                                    data[i].Preferences = account.id;
+                                }
+
+                                for (let j = 0; j < data[i].__parsed_extra.length; j++) {
+                                    if(account.lastName === data[i].__parsed_extra[j] || account.firstName + " " + account.lastName === data[i].__parsed_extra[j] ) {
+                                        data[i].__parsed_extra[j] = account.id;
+                                    } 
+                                }
+                            });
+
+                            for(let k = 0; k < data.length; k++) {
+                                if(data[k].lastName === data[i].Preferences || data[k].firstName + " " + data[k].lastName === data[i].Preferences) {
+                                    data[i].Preferences = ids[k];
+                                }
+
+                                for (let j = 0; j < data[i].__parsed_extra.length; j++) {
+                                    if(data[k].lastName === data[i].__parsed_extra[j] || data[k].firstName + " " + data[k].lastName === data[i].__parsed_extra[j] ) {
+                                        data[i].__parsed_extra[j] = ids[k];
+                                    }
+                                }
+                            }
+
+
+                            preferences = data[i].ExamGrade + "|" + data[i].Preferences + "," + data[i].__parsed_extra.join(",") + "|" + data[i].Specialization;
+                        }
+
+                        await axios.put("https://localhost:7053/api/users/set-users-preferences/" + id  + '/' + preferences);
+                    }
+                    else if(data[i].Role === "Hospital"){
+                        id = ids[i];
+
+                        if(!data[i].hasOwnProperty("__parsed_extra")) {
+                            preferences = data[i].Preferences;
+                        }
+                        else {
+                            preferences = data[i].Preferences + "," + data[i].__parsed_extra.join(",");
+                        }
+
+                        await axios.put("https://localhost:7053/api/users/set-users-preferences/" + id + '/' + preferences);
+                    }
+
+                }
+            }
+
+        });
+        
+    };
+
+    const handleImport = async () => {
+        fileInputRef.current.click();
+    };
+
     const jwtToken = Cookies.get('jwtToken');
     const decodedToken = jwt(jwtToken);
 
@@ -130,7 +286,7 @@ function HomePage() {
                 <div className="mainDiv">
                     <div className="dashboard">
                         <h1>Admin Dashboard</h1>
-                        <div><b>Search for a user:</b> <input type="text" placeholder="Search.." ></input></div>
+                        <div><b>Search for a user:</b> <input type="text" placeholder="Search.." onChange={handleSearchChange}></input></div>
                         <table className="accountsTable">
                             <thead>
                                 <tr>
@@ -140,7 +296,7 @@ function HomePage() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {accounts.map((account) => (
+                                {searchedAccounts.map((account) => (
                                     <tr key={account.lastName}>
                                         <td>{account.firstName} {account.lastName}</td>
                                         <td>{account.role}</td>
@@ -154,7 +310,14 @@ function HomePage() {
                         </table>
 
                         <div className="buttonGroup">
-                            <button className="deadlineBtn">Import Data</button>
+                            <input
+                            type="file"
+                            ref={fileInputRef}
+                            style={{ display: 'none' }}
+                            accept=".csv"
+                            onChange={handleFileSelect}
+                            />
+                            <button className="deadlineBtn" onClick={handleImport}>Import Data</button>
                             <button className="deadlineBtn" onClick={handleSetDeadline}>Set Form Deadline</button>
                             <p><b>Current Deadline: {deadline}</b></p>
                             <select name="algorithms" className="selectBtn">
